e6ecb9e0a189ecb948b7f32b3195240c17505831
Pankaj-Jadhav17 <pankajjadhav1712@gmail.com>
2026-05-07 12:03:22 +0530

    convert km in miles

---
+++ Practice_Code/Function/km.py
@@ -0,0 +1,6 @@
+kilometers = float(input("Enter the distance in kilometer:"))
+conversion_factor = 0.621371
+
+miles = kilometers * conversion_factor
+
+print(f"{kilometers}is equals to {miles} miles")
