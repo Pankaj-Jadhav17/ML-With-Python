14f561b452e8574af7a39b24621dccefe3695afc
Pankaj-Jadhav17 <pankajjadhav1712@gmail.com>
2026-05-21 13:23:30 +0530

    convert number decimal to binary

---
+++ Practice_Code/PDF_Code/DecimalToBinary.py
@@ -0,0 +1,9 @@
+# Write a Python Program to Convert Decimal to Binary, Octal and Hexadecimal.
+# How can we manually convert a decimal number to binary, octal and hexadecimal?
+
+dec_num = int(input("Enter a number: "))
+
+print("The decimal value of ", dec_num, "is:")
+print(bin(dec_num),"in binary.")
+print(oct(dec_num),"in octal.")
+print(hex(dec_num), "in hexadecimal.")
