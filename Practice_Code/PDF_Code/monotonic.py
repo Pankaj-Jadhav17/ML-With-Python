2dba1f58f90f5b43aba2895372e1931a45b53e11
Pankaj-Jadhav17 <pankajjadhav1712@gmail.com>
2026-06-02 20:40:57 +0530

    check given array is monotonic or not

---
+++ Practice_Code/PDF_Code/monotonic.py
@@ -0,0 +1,16 @@
+def monotonic(arr):
+    increasing = decreasing = True
+    for i in range(1, len(arr)):
+        if arr[i] > arr[i-1]:
+            decreasing = False
+        elif arr[i] < arr[i-1]:
+            increasing = False
+    return increasing or decreasing
+
+arr1 = [1, 2, 3, 4, 5]
+arr2 = [5, 4, 3, 2, 1]
+arr3 = [1, 3, 2, 4]
+
+print("Array 1 is monotonic:", monotonic(arr1))
+print("Array 2 is monotonic:", monotonic(arr2))
+print("Array 3 is monotonic:", monotonic(arr3))
