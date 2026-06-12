04f381c012d4ccb8d54563fa572f78783f946f33
Pankaj-Jadhav17 <pankajjadhav1712@gmail.com>
2026-06-12 09:03:30 +0530

    Print sum of array list

---
+++ Practice_Code/PDF_Code/listSum.py
@@ -0,0 +1,7 @@
+# Write a Python program to sum of  all numbers in the list.
+numbers = [1, 2, 3, 4, 5]
+sum_of_numbers = 0
+for i in numbers:
+    sum_of_numbers += i
+print("Sum of all numbers in the list:", sum_of_numbers)
+    
