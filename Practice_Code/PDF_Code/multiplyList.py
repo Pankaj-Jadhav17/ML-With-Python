09635ed5b929fb45125c1feec9cc97b7dc1a279d
Pankaj-Jadhav17 <pankajjadhav1712@gmail.com>
2026-06-13 10:31:17 +0530

    multiply all numbers in the list

---
+++ Practice_Code/PDF_Code/multiplyList.py
@@ -0,0 +1,7 @@
+# Write a Python program to Multiply all numbers in the list.
+
+numbers = [1, 2, 3, 4, 5]
+product_of_numbers = 1
+for number in numbers:
+    product_of_numbers *= number
+print("The product of all numbers in the list is:", product_of_numbers)
