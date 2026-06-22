6e967bac1ccadb1bda33cd51dcc1ac2b5116cb12
Pankaj-Jadhav17 <pankajjadhav1712@gmail.com>
2026-06-22 10:13:19 +0530

    cloning the list

---
+++ Practice_Code/PDF_Code/cloningList.py
@@ -0,0 +1,6 @@
+# Write a Python program to Cloning or Copying a list.
+original_list = [1, 2, 3, 4, 5]
+cloned_list = original_list.copy()
+print("Original List:", original_list)
+print("Cloned List:", cloned_list)
+    
