4316eeaafaf332a40f91ab6b9d8bbd1fc505e709
Pankaj-Jadhav17 <pankajjadhav1712@gmail.com>
2026-06-27 08:43:49 +0530

    Join the strings

---
+++ Practice_Code/PDF_Code/join.py
@@ -0,0 +1,7 @@
+input_str = "Python is a powerful programming language."
+word_list = input_str.split()
+separator = " "
+result_str = separator.join(word_list)
+print(f"Original string: {input_str}")
+print(f"List of words: {word_list}")
+print(f"String after joining words: {result_str}")
